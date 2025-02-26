56215d001f9a2e549019798a95648341e0bf0426
aymericricher <richayme1@gmail.com>
2025-02-26 11:56:00 +0100

    Add files via upload

---
+++ exerciceAD1.ps1
@@ -0,0 +1,36 @@
+﻿#=======================================================
+#NAME:exerciceAD1.ps1
+#AUTHOR: RICHER Aymeric
+#DATE:26/02/2025
+#
+#VERSION:1.0
+#
+#=======================================================
+
+Import-Module ActiveDirectory
+
+while ($true) {
+    $OUName = Read-Host "Entrez le nom de la nouvelle OU"
+    $ParentOU = Read-Host "Entrez le chemin de l'OU parente (laissez vide pour la racine du domaine)"
+    
+ 
+
+    if ($ParentOU -eq ""){
+        try {New-ADOrganizationalUnit -Name $OUName -Path "DC=local,DC=californie,DC=cub,DC=sioplc,DC=fr"}
+        catch {"échec de la création de l'OU";break}
+    }
+    else {
+        try {New-ADOrganizationalUnit -Name $OUName -Path "OU=$ParentOU,DC=local,DC=californie,DC=cub,DC=sioplc,DC=fr"}
+        catch {"échec de la création de l'OU";break}
+    }
+    $Continue = Read-Host "Voulez-vous créer une autre OU ? (O/N)"
+    if ($Continue -ne "O") { break }
+}
+
+
+
+
+
+
+
+
---
+++ exerciceAD2.ps1
@@ -0,0 +1,39 @@
+﻿#=======================================================
+#NAME:exerciceAD1.ps1
+#AUTHOR: RICHER Aymeric
+#DATE:26/02/2025
+#
+#VERSION:1.0
+#
+#=======================================================
+
+Import-Module ActiveDirectory
+
+
+while ($true) {
+    # Demande des informations pour la création de l'utilisateur
+    $Nom = Read-Host "Entrez le nom de l'utilisateur"
+    $Prenom = Read-Host "Entrez le prénom de l'utilisateur"
+    $SamAccountName = Read-Host "Entrez le nom de connexion (sAMAccountName)"
+    $UserPrincipalName = "$SamAccountName@local.californie.cub.sioplc.fr"   
+    $MotDePasse = Read-Host "Entrez le mot de passe" -AsSecureString
+        
+    try{New-ADUser -Name "$Prenom $Nom" `
+               -DisplayName "$Nom $Prenom" `
+               -GivenName $Prenom `
+               -Surname $Nom `
+               -SamAccountName $SamAccountName `
+               -UserPrincipalName $UserPrincipalName `
+               -AccountPassword $MotDePasse `
+               -Enabled $true `
+               -PassThru
+    }
+    catch{"échec de la création de l'utilisateur";break}
+    Write-Host "Utilisateur $Prenom $Nom créé avec succès !" -ForegroundColor Green
+
+    $Continue = Read-Host "Voulez-vous créer une autre OU ? (O/N)"
+    if ($Continue -ne "O") { break }
+}
+
+
+
