7999768661029a41619773ca01dcc42c4ec8db20
Axel Salme <axel.salme@orange.fr>
2025-02-26 09:19:11 +0100

    first commit

---
+++ Sans titre1.ps1
@@ -0,0 +1,26 @@
+﻿#=======================================================
+#NAME:exerciceDNS1.ps1
+#AUTHOR: RICHER Aymeric
+#DATE:26/02/2025
+#
+#VERSION:1.0
+#COMMENTS: Création d'une étentue DHCP avec le nom,l'adresse réseau,le masque,la première adresse,la dernière et la passerelle
+#
+#=======================================================
+
+
+
+$nombre = Read-Host "Combien d'hôtes voulez-vous créer ?"
+
+
+
+
+for ($i = 0; $i -lt $nombre; $i++){
+$nomA = Read-host "Entrer le nom du type A ?"
+$addrA = Read-host "Entrer l'adresse réseau de l'hote ?"
+Add-DnsServerResourceRecordA -Name $nomA -Zone local.californie.cub.sioplc.fr. -IPv4Address $addrA
+}
+
+
+
+
